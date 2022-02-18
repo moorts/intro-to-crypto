d2e325915488d8517dd1bedb955d0e775ec0fc39
moorts <moritz.gutfleisch@gmx.de>
2022-02-18 11:36:25 +0100

    Vigenere

--- fun.py
+++ fun.py
@@ -1,6 +1,17 @@
+class Utils:
+    def shift_char(c, k):
+        if ord(c) < ord('A') or ord(c) > ord('Z'):
+            return c
+        return chr((ord(c) - ord('A') + k) % 26 + ord('A'))
+
+    def mult_char(c, k):
+        if ord(c) < ord('A') or ord(c) > ord('Z'):
+            return c
+        return chr(((ord(c) - ord('A')) * k) % 26 + ord('A'))
+
 class Caesar:
     def encrypt(p, k):
-        return "".join(map(lambda c: chr((ord(c) - ord('A') + k) % 26 + ord('A')), p))
+        return "".join(map(lambda c: Utils.shift_char(c, k), p.upper()))
     def decrypt(c, k):
         return Caesar.encrypt(c, 26-k)
 
@@ -8,7 +19,7 @@ class Multiplicative:
     def encrypt(p, k):
         # k has to have multiplicative inverse
         assert gcd(k, 26) == 1
-        return "".join(map(lambda c: chr(((ord(c) - ord('A')) * k) % 26 + ord('A')), p))
+        return "".join(map(lambda c: Utils.mult_char(c, k), p.upper()))
     def decrypt(c, k):
         # k has to have multiplicative inverse
         assert gcd(k, 26) == 1
@@ -19,12 +30,39 @@ class Affine:
         s, t = k
         # s has to have multiplicative inverse
         assert gcd(s, 26) == 1
-        return "".join(map(lambda c: chr((t + (ord(c) - ord('A')) * s) % 26 + ord('A')), p))
+        return "".join(map(lambda c: Utils.shift_char(Utils.mult_char(c, s), t), p))
     def decrypt(c, k):
         s, t = k
         # s has to have multiplicative inverse
         assert gcd(s, 26) == 1
         return Affine.encrypt(c, (pow(s, -1, 26), 26-t))
+
+class Vigenere:
+    def gen_key(p, k):
+        n = len(p)
+        key = k.upper()
+        i, non_alphabetic = 0, 0
+        while i < n:
+            if not ord('A') <= ord(p[i]) <= ord('Z'):
+                yield -1
+                non_alphabetic += 1
+            else:
+                yield ord(key[(i-non_alphabetic)%len(key)]) - ord('A')
+            i += 1
+
+    def encrypt(p, k):
+        return "".join(map(lambda x: Caesar.encrypt(x[0], x[1]), zip(p, Vigenere.gen_key(p, k))))
+    def decrypt(c, k):
+        return "".join(map(lambda x: Caesar.decrypt(x[0], x[1]), zip(c, Vigenere.gen_key(c, k))))
+
+## Sanity checks
+KEY = "VOGEL"
+plaintext = "HELLO THERE"
+ciphertext = "CSRPZ OVKVP"
+assert Vigenere.decrypt(Vigenere.encrypt(plaintext, KEY), KEY) == plaintext
+assert Vigenere.encrypt(plaintext, KEY) == ciphertext
+assert "".join(map(lambda x: chr(x + ord('A')), Vigenere.gen_key("HELLOL", KEY))) == "VOGELV"
+
 
 print(Caesar.encrypt("KARLSRUHE", 13))
 print(Caesar.decrypt("UPWGGPS", ord("G")-ord("R")))
